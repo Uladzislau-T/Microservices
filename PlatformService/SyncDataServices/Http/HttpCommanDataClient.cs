d4bd68637098afebeaf2c4aacc13d7090febfe04
Uladzislau <logentuch@gmail.com>
2022-12-14 11:40:39 +0300

    adding k8s and command service

---
+++ PlatformService/SyncDataServices/Http/HttpCommanDataClient.cs
@@ -0,0 +1,30 @@
+﻿using PlatformService.Dto;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommanDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public HttpCommanDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto platform)
+        {
+            var httpContent = new StringContent(JsonSerializer.Serialize(platform), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}", httpContent);
+
+            if (response.IsSuccessStatusCode)
+                Console.WriteLine("Everything is OK!!!");
+            else
+                Console.WriteLine("NOT OK!!!");
+        }
+    }
+}
