042330756b1269163b320319d748ace46dd73437
Uladzislau <logentuch@gmail.com>
2022-12-16 14:54:19 +0300

    adding sql server pod and etc

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,18 +1,32 @@
 ﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd)
         {
             using var serviceScope = app.ApplicationServices.CreateScope();
-            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProd)
         {
+            if (isProd)
+            {
+                Console.WriteLine("--> Attempting to apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not run migrations: {ex.Message}");
+                }
+            }
+
             if (!context.Platform.Any())
             {
                 Console.WriteLine("Creating dummy Data");
@@ -20,7 +34,8 @@ namespace PlatformService.Data
                 context.Platform.AddRange(
                     new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
                     new Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
-                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" },
+                    new Platform() { Name = "KubernetesNEW", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
                 );
 
                 context.SaveChanges();
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
@@ -8,40 +10,64 @@ namespace PlatformService
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddDbContext<AppDbContext>(opt => 
-                opt.UseInMemoryDatabase("InMem"));
-
-            builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
-            builder.Services.AddHttpClient<ICommandDataClient, HttpCommanDataClient>();
-
-            builder.Services.AddControllers();
-
-            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            PrepDb.PrepPopulation(app);
-
-            Console.WriteLine($" --> CommandService Endpoint {builder.Configuration["CommandService"]}");
-
-            app.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
+
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        var builder = WebApplication.CreateBuilder(args);
+
+    //        // Add services to the container.
+    //        builder.Services.AddDbContext<AppDbContext>(opt => 
+    //            opt.UseInMemoryDatabase("InMem"));
+
+    //        builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+    //        builder.Services.AddHttpClient<ICommandDataClient, HttpCommanDataClient>();
+
+    //        builder.Services.AddControllers();
+
+    //        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    //        builder.Services.AddEndpointsApiExplorer();
+    //        builder.Services.AddSwaggerGen();
+
+    //        Console.WriteLine($" --> CommandService Endpoint {builder.Configuration["CommandService"]}");
+
+    //        Console.WriteLine("--> Using SqlServer Db");
+    //        builder.Services.AddDbContext<AppDbContext>(opt =>
+    //                opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+
+    //        var app = builder.Build();
+
+    //        //if (app.Environment.IsProduction())
+    //        //{
+
+
+
+    //        //}
+    //        //else
+    //        //{
+    //        //    app.UseSwagger();
+    //        //    app.UseSwaggerUI();
+    //        //}
+
+    //        //app.UseAuthorization();
+
+    //        app.MapControllers();
+
+    //        //PrepDb.PrepPopulation(app, app.Environment.IsProduction());            
+
+    //        app.Run();
+    //    }
+    //}
 }
